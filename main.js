0744935c2828daae5fe583db7d34b51815891fd1
Solitary-Rhyme <2072386241@qq.com>
2023-01-01 18:06:49 +0800

    首页功能

--- main.js
+++ main.js
@@ -3,6 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+//请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:"加载中....."
+	})
+}
+$http.afterRequest = function(options){
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title = '数据加载失败!', duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
