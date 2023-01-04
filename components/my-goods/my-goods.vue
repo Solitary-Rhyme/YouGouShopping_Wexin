44409084508261f9d3b312266dbc4e325f815c15
Solitary-Rhyme <2072386241@qq.com>
2023-01-04 11:37:20 +0800

    商品列表页面开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="goods-item">
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+		</view>
+	
+		<view class="goods-item-right">
+			<view class="goods-name">
+				{{goods.goods_name}}
+			</view>
+			<view class="goods-info-box">
+				<view class="goods-price">
+					￥{{goods.goods_price | toFixed}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			goods:{
+				type: Object,
+				default: {}
+			}
+		},
+		data() {
+			return {
+				defaultPic: 'https://hbimg.b0.upaiyun.com/5d91b400204034583e9c24dd4b10f225f3e59522ed9-zXPxPI_fw658'
+			};
+		},
+		filters:{
+			toFixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+
+		.goods-item-left {
+			margin-right: 5px;
+
+			.goods-pic {
+				height: 100px;
+				width: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-info-box {
+				.goods-price {
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		Goods_list
+		<view class="goods-list">
+			<view v-for="(goods,index) in goodsList" :key="index" @click="goToDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,65 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isLoading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(cb) {
+				
+				this.isLoading = true
+				
+				const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				
+				this.isLoading = false
+				
+				cb && cb()
+				
+				console.log(res);
+				//请求失败
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				//请求成功
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			goToDetail(goods){
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据已加载完毕')
+			
+			if(this.isLoading) return
+			
+			this.queryObj.pagenum += 1
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isLoading = false
+			this.goodsList = []
+			
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
